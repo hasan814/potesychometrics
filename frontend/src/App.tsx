258b100b3e270da7370bd80b6aaa305ae0c0b84e
hasan814 <h.mousavi910@gmail.com>
2024-11-13 12:46:07 +0330

    Added Sign up Functionality

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,11 @@
 import { Outlet } from "react-router-dom";
 import Header from "./components/layouts/Header";
+import { Toaster } from "react-hot-toast";
 
 const App = () => {
   return (
     <>
+      <Toaster />
       <Header />
       <main>
         <Outlet />
--- frontend/src/components/templates/SignUpPage.tsx
+++ frontend/src/components/templates/SignUpPage.tsx
@@ -1,17 +1,64 @@
+import { useState, ChangeEvent, FormEvent } from "react";
 import { Button, Label, TextInput } from "flowbite-react";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import { BeatLoader } from "react-spinners";
+import { FormData } from "../../types/Auth";
 
-const SignUpPage = () => {
+import toast from "react-hot-toast";
+
+const SignUpPage: React.FC = () => {
+  // ============== Navigate ====================
+  const navigate = useNavigate();
+
+  // ============== State ====================
+  const [formData, setFormData] = useState<FormData>({});
+  const [loading, setIsLoading] = useState(false);
+
+  // ============== Change Function ====================
+  const changeHandler = (event: ChangeEvent<HTMLInputElement>) => {
+    const { id, value } = event.target;
+    setFormData((prevData) => ({ ...prevData, [id]: value.trim() }));
+  };
+
+  // ============== Submit Function ====================
+  const submitHandler = async (event: FormEvent) => {
+    event.preventDefault();
+    setIsLoading(true);
+    try {
+      const response = await fetch(`/api/auth/signup`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(formData),
+      });
+
+      const responseData = await response.json();
+      console.log(responseData);
+      if (response.ok) {
+        toast.success("Sign up Successfully");
+        navigate("/sign-in");
+      } else {
+        toast.error(responseData.message);
+        setIsLoading(false);
+      }
+    } catch (error) {
+      const errorMessage =
+        error instanceof Error ? error.message : "An unknown error occured";
+      toast.error(`Signup error: ${errorMessage}`);
+      setIsLoading(false);
+    }
+  };
+
+  // ============== Rendering ====================
   return (
     <div className="min-h-screen mt-20">
       <div className="gap-5 flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center">
         <div className="flex-1">
           <Link
-            to={"/"}
-            className=" text-4xl sm:text-xl font-bold dark:text-white"
+            to="/"
+            className="text-4xl sm:text-xl font-bold dark:text-white"
           >
             <span className="rounded-lg px-2 py-1 bg-gradient-to-r from-indigo-500 via-purple-500 to-pink-500 text-white">
-              Potesychometrics
+              Potepsychometrics
             </span>
           </Link>
           <p className="text-sm mt-5">
@@ -20,33 +67,42 @@ const SignUpPage = () => {
           </p>
         </div>
         <div className="flex-1">
-          <form className="flex flex-col gap-4">
+          <form onSubmit={submitHandler} className="flex flex-col gap-4">
             <div>
               <Label value="Your Name :" />
-              <TextInput type="text" placeholder="Username" id="username" />
+              <TextInput
+                type="text"
+                id="username"
+                placeholder="Username"
+                onChange={changeHandler}
+              />
             </div>
             <div>
               <Label value="Your Email :" />
               <TextInput
                 type="email"
-                placeholder="name@comapny.com"
                 id="email"
+                onChange={changeHandler}
+                placeholder="name@comapny.com"
               />
             </div>
             <div>
               <Label value="Your Password :" />
-              <TextInput type="password" placeholder="Password" id="password" />
+              <TextInput
+                id="password"
+                type="password"
+                placeholder="Password"
+                onChange={changeHandler}
+              />
             </div>
-            <Button gradientDuoTone="purpleToPink" type="submit">
-              Sign Up
-            </Button>
+            {loading ? (
+              <BeatLoader color="#1cd50f" className="mx-auto" />
+            ) : (
+              <Button gradientDuoTone="purpleToPink" type="submit">
+                Sign Up
+              </Button>
+            )}
           </form>
-          <div className="flex gap-2 text-sm mt-5">
-            <span>Have an account?</span>
-            <Link to={"/sign-in"} className="text-blue-500">
-              Sign In
-            </Link>
-          </div>
         </div>
       </div>
     </div>
